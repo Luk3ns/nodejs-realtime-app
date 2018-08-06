ee99fd168aec7cdf7a5b4bc922a9f24816ff5f11
Lukens <33292417+Luk3ns@users.noreply.github.com>
2018-08-06 10:58:25 +0200

    nodejs server

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 var express = require("express");
-var app = express();
+var app = express(); //express init
 var server = require("http").createServer(app);
 var io = require("socket.io")(server);
 
@@ -7,7 +7,8 @@ var io = require("socket.io")(server);
 //  res.sendFile(__dirname + "/public/index.html");
 //});
 
-app.use(express.static("public"));
+//Serving static files in Express
+app.use(express.static("public")); 
 
 io.on("connection", function(client) {
   console.log("Client connected...");
@@ -16,7 +17,7 @@ io.on("connection", function(client) {
     console.log(data);
   });
 
-  client.on("messages", function(data) {
+  client.on("rtdata", function(data) {
     client.emit("thread", data);
     client.broadcast.emit("thread", data);
   });
