fd13d8c0d55b80439032a4c22c265c44cfd87cdd
Lukens <33292417+Luk3ns@users.noreply.github.com>
2018-09-06 19:37:38 +0200

    Update app.js

--- app.js
+++ app.js
@@ -3,10 +3,6 @@ var app = express(); //express init
 var server = require("http").createServer(app);
 var io = require("socket.io")(server);
 
-//app.get("/", function(req, res, next) {
-//  res.sendFile(__dirname + "/public/index.html");
-//});
-
 //Serving static files in Express
 app.use(express.static("public")); 
 
